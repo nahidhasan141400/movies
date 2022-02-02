f5a00c3562fc81aff414dc62e356abfa8799a217
NahidPc <nahidhasan141400@gmail.com>
2022-02-02 15:00:41 +0600

    add lenguage to data and preview to buttom op player ,,,,,,

--- src/main.js
+++ src/main.js
@@ -23,7 +23,7 @@ async function  data(){
         sour.innerHTML += `<div class="block">
         <h1>${da[i].name}</h1>
         <h2>${da[i].size}</h2>
-        ${da[i].paly ? ` <button id='play' onclick="play('${da[i].link}')"><img src="src/img/play.png" alt=""></button>` : ` <a id='down' href="${da[i].link}">Download</a>`}
+        ${da[i].paly ? ` <button id='play' onclick="play('${da[i].link}','${da[i].language}')"><img src="src/img/play.png" alt=""></button>` : ` <a id='down' href="${da[i].link}">Download</a>`}
         <img id='thumb' src="${da[i].img}" alt="" />
       </div>`
         
@@ -44,7 +44,7 @@ async function  data(){
         sour.innerHTML += `<div class="block">
         <h1>${da[i].name}</h1>
         <h2>${da[i].size}</h2>
-        ${da[i].paly ? ` <button id='play' onclick="play('${da[i].link}')"><img src="src/img/play.png" alt=""></button>` : ` <a id='down' href="${da[i].link}">Download</a>`}
+        ${da[i].paly ? ` <button id='play' onclick="play('${da[i].link}','${da[i].language}')"><img src="src/img/play.png" alt=""></button>` : ` <a id='down' href="${da[i].link}">Download</a>`}
         <img id='thumb' src="${da[i].img}" alt="" />
       </div>`
         
@@ -69,10 +69,11 @@ async function qury_movie (value){
     if( v.indexOf(key) !== -1){
       newdata.push(data2[i]);
       // console.log(v);
+      // console.log(` macth ${v} , with ${key}`)
     
     }
     else{
-      console.log(`not macth ${v} , with ${key}`)
+      // console.log(`not macth ${v} , with ${key}`)
       // console.log(key);
     }
 
@@ -101,28 +102,37 @@ async function qury_movie (value){
   
 // }
 
-
-search.addEventListener('keyup', 
-        async ()=>{
-          let data3 = await qury_movie(search.value);
-          ro.innerHTML='';
-          let n ;
-          if( data3.length > 10 ){
-            n = 10;
-          }else{
-            n = data3.length
-          }
-          
-          for (let i = 0; i < n; i++) {
-            ro.innerHTML += `<div class="block">
-            <h1>${data3[i].name}</h1>
-            <h2>${data3[i].size}</h2>
-            ${data3[i].paly ? ` <button id='play' onclick="play('${data3[i].link}')"><img src="src/img/play.png" alt=""></button>` : ` <a id='down' href="${data3[i].link}">Download</a>`}
-            <img id='thumb' src="${data3[i].img}" alt="" />
-          </div>`
-            
+let red =  async (data3 , ti)=>{
+  console.log(data3);
+  debugger
+  ti.innerHTML='';
+  let n ;
+  if( data3.length > 10 ){
+    n = 10;
+  }else{
+    n = data3.length
+  }
+  
+  for (let i = 0; i < n; i++) {
+              ti.innerHTML += `<div class="block">
+              <h1>${data3[i].name}</h1>
+              <h2>${data3[i].size}</h2>
+              ${data3[i].paly ? ` <button id='play' onclick="play('${data3[i].link}','${data3[i].language}')"><img src="src/img/play.png" alt=""></button>` : ` <a id='down' href="${data3[i].link}">Download</a>`}
+              <img id='thumb' src="${data3[i].img}" alt="" />
+            </div>`
+              
         }
-      })
+}
+
+
+async function key (list , it){
+  let data3 = await qury_movie(list);
+  red(data3 , it);
+}
+
+search.addEventListener('keyup',()=>{
+  key(search.value, ro);
+} )
 
       let verti = document.querySelector('.verti');
       strNew(verti , 9)
@@ -131,7 +141,7 @@ search.addEventListener('keyup',
       let body = document.querySelector('body');
 
 
-function play (link){
+function play (link, qdata){
 
 
   body.innerHTML=` <div id='blo'></div><iframe id="player"
@@ -140,5 +150,8 @@ function play (link){
   allowfullscreen
 ></iframe><div id='gap'></div><div id="ro2" class="box"></div><script src="src/view.js"></script>`
 let ro2 = document.querySelector('#ro2')
-strNew(ro2 ,6)
-}
+// strNew(ro2 ,6)
+console.log(qdata);
+key(qdata ,ro2);
+ 
+};
